7a97928990eb6bbf591fe687368d3bcff12f2ec2
coder-adnan <adnanmaneri2015@gmail.com>
2024-04-28 12:49:13 -0300

    new-changes

--- db/index.js
+++ db/index.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
+      // useUnifiedTopology: true,
+      // useNewUrlParser: true,
     });
     console.log(
       `\n MongoDB Connected!! DB Host: ${connectionInstance.connection.host}`
